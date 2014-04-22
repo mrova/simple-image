41c78737f0bdac38271a96248feebe102e2e8240
mrova <michal.mrowiec@simplesoft.pl>
2014-04-22 12:03:54 +0200

    qwe

--- Module.php
+++ Module.php
@@ -1,62 +1,46 @@
 <?php
 /**
- * SimpleSoft (http://simplesoft.pl)
+ * Zend Framework (http://framework.zend.com/)
  *
- * @link      https://github.com/mrova/simple-image
- * @copyright Copyright (c) 2014 SimpleSoft (http://simplesoft.pl)
- * @license   New BSD License
+ * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace SimpleImage;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
 
-/**
- * SimpleImage Module for Zend Framework 2
- *
- * @author Michal Mrowiec <michal.mrowiec@simplesoft.pl>
- */
-class Module implements
-	AutoloaderProviderInterface,
-	ConfigProviderInterface,
-	ServiceProviderInterface
+class Module implements AutoloaderProviderInterface
 {
-
 	public function getAutoloaderConfig()
 	{
 		return array(
-				'Zend\Loader\ClassMapAutoloader' => array(
-						__DIR__ . '/autoload_classmap.php',
+			'Zend\Loader\ClassMapAutoloader' => array(
+				__DIR__ . '/autoload_classmap.php',
+			),
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+			// if we're in a namespace deeper than one level we need to fix the \ in the path
+					__NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__),
 				),
-				'Zend\Loader\StandardAutoloader' => array(
-						'namespaces' => array(
-								__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-						),
-				),
+			),
 		);
 	}
 
-	public function getConfig($env = null)
+	public function getConfig()
 	{
 		return include __DIR__ . '/config/module.config.php';
 	}
 
-	/**
-	 * @return array
-	 */
-// 	public function getViewHelperConfig()
-// 	{
-// 		return array(
-// 			'factories' => array(
-// 				'image' => function ($sm) {
-// 					$locator = $sm->getServiceLocator();
-// 					$viewHelper = new View\Helper\Image;
-// 					return $viewHelper;
-// 				}
-// 			),
-// 		);
-
-// 	}
+	public function onBootstrap(MvcEvent $e)
+	{
+		// You may not need to do this if you're doing it elsewhere in your
+		// application
+		$eventManager        = $e->getApplication()->getEventManager();
+		$moduleRouteListener = new ModuleRouteListener();
+		$moduleRouteListener->attach($eventManager);
+	}
 }
--- config/module.config.php
+++ config/module.config.php
@@ -1,19 +1,3 @@
 <?php
-/**
- * SimpleSoft (http://simplesoft.pl)
- *
- * @link      https://github.com/mrova/simple-image
- * @copyright Copyright (c) 2014 SimpleSoft (http://simplesoft.pl)
- * @license   New BSD License
- */
-
-/**
- * Do not write your custom settings into this file
- */
 return array(
-	'view_manager' => array(
-		'template_path_stack' => array(
-			'SimpleImage' => __DIR__ . '/../view',
-		),
-	)
 );
