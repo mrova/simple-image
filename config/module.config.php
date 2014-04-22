62c95f45f5da42ffe4138ea8b88c7f03c7f0c99f
mrova <michal.mrowiec@simplesoft.pl>
2014-04-22 09:33:58 +0200

    reorganize

---
+++ config/module.config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Image (http://simplesoft.pl)
+ *
+ * @link        https://github.com/mrova/simple-image
+ * @copyright   Copyright (c) 2014 SimpleSoft (http://simplesoft.pl)
+ * @author      Michal Mrowiec <michal.mrowiec@simplesoft.pl>
+ * @license     New BSD License
+ */
+
+/**
+ * Do not write your custom settings into this file
+ */
+return array(
+// 	'di' => array(
+// 		'definition' => array(
+// 			'compiler' => array(
+// 				__DIR__ . '/../data/di/definition.php',
+// 			),
+// 		),
+// 		'instance' => array(
+// 			'alias' => array(
+// 			   'WebinoImageThumb' => 'WebinoImageThumb\WebinoImageThumb',
+// 		   ),
+// 		),
+// 	),
+);
