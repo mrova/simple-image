089bf1eab77d9145273b5e5d49007e736df49597
mrova <michal.mrowiec@simplesoft.pl>
2014-04-22 12:51:13 +0200

    .

---
+++ src/SimpleImage/View/Helper/Image.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SimpleImage\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+class Image extends AbstractHelper
+{
+	public function __invoke()
+	{
+		return 'ImageViewHelper::GOGOGO!';
+	}
+}
